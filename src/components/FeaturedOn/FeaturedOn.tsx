423b8812c505ccb10d0992d7acbd3372f9ebfedb
kodeartisan <kodeartisan@gmail.com>
2025-12-27 15:43:55 +0700

    wip

--- src/components/FeaturedOn/FeaturedOn.tsx
+++ src/components/FeaturedOn/FeaturedOn.tsx
@@ -290,6 +290,13 @@ const PRIMARY_BADGES = [
     width: 150,
     height: 54,
   },
+  {
+    src: "https://startupvessel.com/assets/images/badge-dark.png",
+    alt: "Startup Vessel",
+    href: "https://startupvessel.com",
+    width: 150,
+    height: 54,
+  },
 ];
 
 // Secondary badges - shown when "And More" is clicked
