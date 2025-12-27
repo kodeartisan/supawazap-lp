027f5a2aec7e2a36c8fbcb62c4d14456ea94a826
kodeartisan <kodeartisan@gmail.com>
2025-12-27 08:03:44 +0700

    wip

--- src/components/FeaturedOn/FeaturedOn.tsx
+++ src/components/FeaturedOn/FeaturedOn.tsx
@@ -185,6 +185,13 @@ const PRIMARY_BADGES = [
     width: 150,
     height: 54,
   },
+  {
+    src: "https://mystarttools.com/assets/images/badge-dark.png",
+    alt: "My Start Tools",
+    href: "https://mystarttools.com",
+    width: 150,
+    height: 54,
+  },
 ];
 
 // Secondary badges - shown when "And More" is clicked
