8faa90a8b98406fbc47218ef38366094dd24b135
kodeartisan <kodeartisan@gmail.com>
2025-12-27 15:48:06 +0700

    wip

--- src/components/FeaturedOn/FeaturedOn.tsx
+++ src/components/FeaturedOn/FeaturedOn.tsx
@@ -297,6 +297,13 @@ const PRIMARY_BADGES = [
     width: 150,
     height: 54,
   },
+  {
+    src: "https://startupaideas.com/assets/images/badge-dark.png",
+    alt: "Startup AIdeas",
+    href: "https://startupaideas.com",
+    width: 150,
+    height: 54,
+  },
 ];
 
 // Secondary badges - shown when "And More" is clicked
