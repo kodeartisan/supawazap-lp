a95d0c9ded81ef9afbdee92c9278833326c36811
kodeartisan <kodeartisan@gmail.com>
2025-12-28 08:12:43 +0700

    wip

--- src/components/FeaturedOn/FeaturedOn.tsx
+++ src/components/FeaturedOn/FeaturedOn.tsx
@@ -479,6 +479,13 @@ const PRIMARY_BADGES = [
     width: 150,
     height: 54,
   },
+  {
+    src: "https://toolsignal.com/assets/images/badge-dark.png",
+    alt: "Tool Signal",
+    href: "https://toolsignal.com",
+    width: 150,
+    height: 54,
+  },
 ];
 
 // Secondary badges - shown when "And More" is clicked
