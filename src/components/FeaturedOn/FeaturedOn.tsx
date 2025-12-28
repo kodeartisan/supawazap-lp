ca632d598009a85b0c33bb217cc25db7972e7803
kodeartisan <kodeartisan@gmail.com>
2025-12-28 08:01:28 +0700

    wip

--- src/components/FeaturedOn/FeaturedOn.tsx
+++ src/components/FeaturedOn/FeaturedOn.tsx
@@ -451,6 +451,13 @@ const PRIMARY_BADGES = [
     width: 150,
     height: 54,
   },
+  {
+    src: "https://unitelist.com/assets/images/badge-dark.png",
+    alt: "Unite List",
+    href: "https://unitelist.com",
+    width: 150,
+    height: 54,
+  },
 ];
 
 // Secondary badges - shown when "And More" is clicked
