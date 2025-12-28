439d16701db2aced45fc0d0103801e08334aa55b
kodeartisan <kodeartisan@gmail.com>
2025-12-28 07:57:55 +0700

    wip

--- src/components/FeaturedOn/FeaturedOn.tsx
+++ src/components/FeaturedOn/FeaturedOn.tsx
@@ -444,6 +444,13 @@ const PRIMARY_BADGES = [
     width: 150,
     height: 54,
   },
+  {
+    src: "https://weliketools.com/assets/images/badge-dark.png",
+    alt: "We Like Tools",
+    href: "https://weliketools.com",
+    width: 150,
+    height: 54,
+  },
 ];
 
 // Secondary badges - shown when "And More" is clicked
