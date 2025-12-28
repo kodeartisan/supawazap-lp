4797531f192a34fd256972d96fe0c5e5e334008a
kodeartisan <kodeartisan@gmail.com>
2025-12-28 09:27:17 +0700

    wip

--- src/components/FeaturedOn/FeaturedOn.tsx
+++ src/components/FeaturedOn/FeaturedOn.tsx
@@ -514,6 +514,13 @@ const PRIMARY_BADGES = [
     width: 150,
     height: 54,
   },
+  {
+    src: "https://launchscroll.com/assets/images/badge-dark.png",
+    alt: "Launch Scroll",
+    href: "https://launchscroll.com",
+    width: 150,
+    height: 54,
+  },
 ];
 
 // Secondary badges - shown when "And More" is clicked
